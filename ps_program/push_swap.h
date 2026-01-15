c7a24940480662aea56ae66ec565581be279d96f
zakariae ahrabare <zahrabar@c2r7p9.1337.ma>
2026-01-15 20:06:17 +0100

    merge push

--- ps_program/push_swap.h
+++ ps_program/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: zahrabar <zahrabar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 18:01:09 by zahrabar          #+#    #+#             */
-/*   Updated: 2026/01/13 19:16:58 by zahrabar         ###   ########.fr       */
+/*   Updated: 2026/01/14 19:14:59 by zahrabar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,15 @@ typedef struct s_list
 }                   n_list;
 
 
-// manipulate the list
 n_list  *add_new_node(int content);
 void    add_to_back(n_list **list, n_list *node);
 void    add_to_front(n_list **list, n_list *node);
 int     list_size(n_list *list);
-// validation of input
 char    **ft_split(char *str);
 int     ft_atoi(char *str, int *error);
 void	free_all_space(char **arr);
 int     parse_create_list(char *arg, n_list **a_head);
 int     check_dup(n_list *a_head);
-// moves prototype
 void    swap(n_list **h_stack, char w_stack);
 void    swap_both(n_list **a_stack, n_list **b_stack);
 void    push(n_list **src_stack, n_list **dst_stack, char w_stack);
@@ -47,15 +44,10 @@ void    rotate(n_list **h_stack, char w_stack);
 void    rotate_both(n_list **a_stack, n_list **b_stack);
 void    rev_rotate(n_list **h_stack, char w_stack);
 void    rev_rotate_both(n_list  **a_stack, n_list   **b_stack);
-// implemetation
 void    handle_list_size(n_list **a_stack, n_list **b_stack);
 int     is_sorted(n_list    **a_stack);
-
 void    index_nodes(n_list **a_stack, int size);
-
-// big sorting algo
 void    chunks_sort(n_list **a_stack, n_list **b_stack, int size);
-
 
 
 
